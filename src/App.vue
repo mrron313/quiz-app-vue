7fc4a5231b55f4f6aa788c5e4bd8b1d40f01f6ac
Arif-Ul-Islam <arifulislam@bs-23.net>
2019-07-16 15:16:13 +0600

    login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-app>    
+  <v-app>
     <v-toolbar app>
       <v-toolbar-title>Quiz App</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn @click="login">Login</v-btn>
     </v-toolbar>
     <v-content>
       <v-container fluid>
@@ -14,44 +15,19 @@
 </template>
 
 <script>
+
+import { mapActions } from 'vuex';
 export default {
-  name: 'app'
-}
+  name: 'app',
+  methods: {
+    ...mapActions('user', {
+      login: 'login'
+    })
+  }
+};
+
 </script>
 
 <style>
-body {
-  margin: 0;
-}
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
